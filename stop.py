2f6c4259d8c135ef4bba433c5147893ac5a549d0
jaivic <jaivicx10a@gmail.com>
2019-12-16 06:28:57 -0500

    stop function

---
+++ stop.py
@@ -0,0 +1,19 @@
+import json
+import datetime
+import boto3
+
+region = 'us-east-1'
+
+instances = ['i-0f41e6157d37ec40e']
+
+def lambda_handler(event, context):
+    ec2 = boto3.client('ec2', region_name=region)
+    ec2.stop_instances(InstanceIds=instances)
+
+    data = {
+        'output': 'start instances'+ str(instances),
+        'timestamp': datetime.datetime.utcnow().isoformat()
+    }
+    return {'statusCode': 200,
+            'body': json.dumps(data),
+            'headers': {'Content-Type': 'application/json'}}
